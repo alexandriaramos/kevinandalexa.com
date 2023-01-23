c0ec280c0f5fc4272d371534ee0e0397a624c4b0
Alexandria Ramos <alexandriameisramos@gmail.com>
2023-01-23 10:07:53 -0800

    feat: add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,26 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Mousewheel, Pagination } from 'swiper';
-import Card from './Card';
-import analytics from './analytics';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from 'react-router-dom';
+import SaveTheDate from './SaveTheDate';
 
-import 'swiper/css';
-import 'swiper/css/bundle';
-import 'swiper/css/pagination';
 import './App.css';
 
 function App() {
-  const slides = [
+  const router = createBrowserRouter([
     {
-      id: 'Date',
-      render: (key: number) => (
-        <SwiperSlide key={key}>
-          <Card
-            title="kevin + alexa"
-            description="january 13, 2024"
-          />
-        </SwiperSlide>
-      ),
+      path: 'save-the-date',
+      element: <SaveTheDate />,
     },
     {
-      id: 'Ceremony',
-      render: (key: number) => (
-        <SwiperSlide key={key}>
-          <Card
-            title="st. benedict"
-            type="ceremony"
-            description="silang, cavite"
-          />
-        </SwiperSlide>
-      ),
+      path: '*',
+      element: <Navigate to="save-the-date" replace />,
     },
-    {
-      id: 'Reception',
-      render: (key: number) => (
-        <SwiperSlide key={key}>
-          <Card
-            title="narra hill"
-            type="reception"
-            description="laurel, batangas"
-          />
-        </SwiperSlide>
-      ),
-    },
-  ];
+  ]);
 
   return (
-    <Swiper
-      direction="vertical"
-      modules={[Mousewheel, Pagination]}
-      mousewheel={true}
-      onSlideChange={(swiper) => {
-        const slideId = slides[swiper.realIndex].id;
-        if (slideId) {
-          analytics.viewSlide(slideId);
-        }
-      }}
-    >
-      {slides.map((slide, key) => slide.render(key))}
-    </Swiper>
+    <RouterProvider router={router} />
   );
 }
 
---
+++ src/SaveTheDate.tsx
@@ -0,0 +1,67 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Mousewheel, Pagination } from 'swiper';
+import Card from './Card';
+import analytics from './analytics';
+
+import 'swiper/css';
+import 'swiper/css/bundle';
+import 'swiper/css/pagination';
+import './SaveTheDate.css';
+
+function SaveTheDate() {
+  const slides = [
+    {
+      id: 'Date',
+      render: (key: number) => (
+        <SwiperSlide key={key}>
+          <Card
+            title="kevin + alexa"
+            description="january 13, 2024"
+          />
+        </SwiperSlide>
+      ),
+    },
+    {
+      id: 'Ceremony',
+      render: (key: number) => (
+        <SwiperSlide key={key}>
+          <Card
+            title="st. benedict"
+            type="ceremony"
+            description="silang, cavite"
+          />
+        </SwiperSlide>
+      ),
+    },
+    {
+      id: 'Reception',
+      render: (key: number) => (
+        <SwiperSlide key={key}>
+          <Card
+            title="narra hill"
+            type="reception"
+            description="laurel, batangas"
+          />
+        </SwiperSlide>
+      ),
+    },
+  ];
+
+  return (
+    <Swiper
+      direction="vertical"
+      modules={[Mousewheel, Pagination]}
+      mousewheel={true}
+      onSlideChange={(swiper) => {
+        const slideId = slides[swiper.realIndex].id;
+        if (slideId) {
+          analytics.viewSlide(slideId);
+        }
+      }}
+    >
+      {slides.map((slide, key) => slide.render(key))}
+    </Swiper>
+  );
+}
+
+export default SaveTheDate;
